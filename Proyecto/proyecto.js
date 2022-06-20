7cb397c84493ce426ef865561e0105a069ed625a
valeriatoledo12 <107891463+valeriatoledo12@users.noreply.github.com>
2022-06-20 16:42:14 -0500

    Add files via upload

---
+++ Proyecto/proyecto.js
@@ -0,0 +1,65 @@
+function mostrarfigura()
+{
+	var r=document.getElementById("alimento").value;
+
+	switch(r)
+	{
+		case '1':
+		document.getElementById("platillo").innerHTML="1";
+		document.getElementById("imagen-platillo").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
+		case '1':
+		document.getElementById("figura-titulo").innerHTML="chilaquiles verdes";
+		document.getElementById("figura-imagen").src="imagenes/chilaquiles verdes.jpg";
+		break;
+		
